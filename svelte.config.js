d1937f5d7fae7e1bf54f3263fa364b02728d9ba9
jackra1n <45038833+jackra1n@users.noreply.github.com>
2024-08-15 00:05:34 +0200

    Fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
+const BASE_URL = process.env.BASE_URL || "";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,16 +11,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/swiss-cities-map' : ''
+			base: BASE_URL
 		}
 	}
 };
