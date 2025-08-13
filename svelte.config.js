0cbcc5bfda8ea72dc897244256fa6bc1b5d74b04
jackra1n <45038833+jackra1n@users.noreply.github.com>
2025-08-13 22:10:26 +0200

    update project and dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
-const BASE_URL = process.env.BASE_URL || "";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: BASE_URL
-		}
-	}
+	kit: { adapter: adapter() }
 };
 
 export default config;
